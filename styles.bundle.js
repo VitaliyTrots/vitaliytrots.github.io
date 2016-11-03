7912e6f723ce49fe3c54793273525763afcbcccb
Vitaliy Trots <vitaliy.trots@valor-software.com>
2016-11-03 15:56:19 +0200

    Updates

--- styles.bundle.js
+++ styles.bundle.js
@@ -1,6 +1,6 @@
 webpackJsonp([1,2],{
 
-/***/ 304:
+/***/ 305:
 /***/ function(module, exports, __webpack_require__) {
 
 // style-loader: Adds some css to the DOM by adding a <style> tag
@@ -9,7 +9,7 @@ webpackJsonp([1,2],{
 var content = __webpack_require__(541);
 if(typeof content === 'string') content = [[module.i, content, '']];
 // add the styles to the DOM
-var update = __webpack_require__(564)(content, {});
+var update = __webpack_require__(583)(content, {});
 if(content.locals) module.exports = content.locals;
 // Hot Module Replacement
 if(false) {
@@ -99,7 +99,7 @@ module.exports = function() {
 
 /***/ },
 
-/***/ 564:
+/***/ 583:
 /***/ function(module, exports) {
 
 /*
@@ -352,13 +352,13 @@ function updateLink(linkElement, obj) {
 
 /***/ },
 
-/***/ 567:
+/***/ 586:
 /***/ function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(304);
+module.exports = __webpack_require__(305);
 
 
 /***/ }
 
-},[567]);
+},[586]);
 //# sourceMappingURL=styles.map
